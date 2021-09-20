5e03b50187b9f955cc400a8b3eb7711449f5eaca
Luca Graves <Lgrave264@west-mec.local>
2021-09-20 13:47:55 -0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App(){
+    
+    return(
+        <main>
+            <section className='container'>
+                <p>content</p>
+            </section>
+        </main>
+    )
+}
+
+export default App
