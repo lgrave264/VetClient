ffcdecba022f537a424678e8a381d351cfeebb82
chazcoffdude <71471786+chazcoffdude@users.noreply.github.com>
2021-09-30 13:36:13 -0700

    filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import data from './data';
+import Filter from './Components/Filter';
 import Pet from './Components/Pet';
 
-function App(){
+function App() {
     const pets = data;
 
-    return(
+
+
+
+    return ( 
         <>
-            <h1 style={{textAlign: 'center'}}>Here are our pets!</h1>
+            <h1 style={{ textAlign: 'center' }}>Here are our pets!</h1>
+            
+
             <hr />
+            <Filter />
+            
             <div id='pets'>
+                
                 {
                     pets.map((pet) => {
+                        
                         return <Pet data={pet} key={pet.id} />
                     })
+
+                    
+                    
                 }
             </div>
+            
         </>
     )
 }
